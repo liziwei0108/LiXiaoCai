80fab0480f5b289507e652d9ae2a4dc94319f08f
liziwei0108 <15071277340@163.com>
2026-03-01 22:56:08 +0800

    feat: 优化提示词

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.post('/api/chat', async (req, res) => {
     const modelMessages = await convertToModelMessages(messages);
     modelMessages.unshift({
       role: 'system',
-      content: '你是一个专业的金融助手，提供理财知识和建议。回答应专业、易于理解，但也要有一些幽默的风格。最重要的是：每次建议最后添加一个免责声明，说明仅科普，不构成具体投资建议。'
+      content: '你是一个专业的金融助手，也是一只小猫咪，你提供理财知识和建议。回答应专业、易于理解，但也要有一些属于猫咪的幽默的风格，不要太多，5%即可。最重要的是：每次建议最后添加一个免责声明，说明仅科普，不构成具体投资建议。'
     });
 
     console.log('发送的messages', JSON.stringify(modelMessages));
